60b730cfcf2458487fed9f8ba879255a993f1d96
Unknown <kylehinckley@hotmail.com>
2019-02-19 00:44:04 -0700

    Meowschwitz Vertical Slice Project

---
+++ Meowschwitz/Assets/Scripts/TimeChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeChanger : MonoBehaviour
+{
+
+	private Transform SunRotation;
+	public float TimeScale;
+
+	void Start()
+	{
+		SunRotation = GetComponent<Transform>();
+	}
+	
+	void Update ()
+	{
+		SunRotation.Rotate(TimeScale * Time.deltaTime, 0, 0);
+	}
+}
