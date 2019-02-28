19e1c856fbb27307d39dfd9f20a2b37acac90237
Unknown <kylehinckley@hotmail.com>
2019-02-28 08:21:05 -0700

    Meowschwitz Vertical Slice 3 Mechanics Work

---
+++ Meowschwitz/Assets/Scripts/Jakob/Jump.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+
+	private CharacterController controller;
+	private Vector3 position;
+	private float jumpStart;
+	
+	public float MoveSpeed = 5;
+	public float Gravity = 1;
+	public float JumpValue = 20;
+	public float JumpCount = 2;
+	
+	void Start ()
+	{
+		jumpStart = JumpCount;
+		controller = GetComponent<CharacterController>();
+	}
+	
+	void Update ()
+	{
+		position.x = Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
+		if (JumpCount > 1 && Input.GetButton("Jump"))
+		{
+			JumpCount--;
+			position.y = JumpValue * Time.deltaTime;
+		}
+
+		if (controller.isGrounded)
+		{
+			JumpCount = jumpStart;
+		}
+
+		position.y -= Gravity * Time.deltaTime;
+		controller.Move(position);
+	}
+}
---
+++ Meowschwitz/Assets/Scripts/Jakob/MoveCharacter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+
+	private CharacterController controller;
+	private Vector3 position;
+	private float jumpStart;
+	
+	public float MoveSpeed = 5;
+	public float Gravity = -9.81f;
+	public float JumpValue = 200;
+	public float JumpCount = 2;
+	
+	void Start ()
+	{
+		jumpStart = JumpCount;
+		controller = GetComponent<CharacterController>();
+	}
+	
+	void Update ()
+	{
+		position.x = Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
+		
+		if (JumpCount > 0 && Input.GetButtonDown("Jump"))
+		{
+			JumpCount--;
+			position.y = JumpValue * Time.deltaTime;
+		}
+
+		if (controller.isGrounded)
+		{
+			JumpCount = jumpStart;
+		}
+
+		position.y += Gravity * Time.deltaTime;
+		controller.Move(position);
+	}
+	
+}
