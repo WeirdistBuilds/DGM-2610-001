d3ca36fafef5a023d7051ebfb0ba684d1f865f30
Unknown <kylehinckley@hotmail.com>
2019-03-14 10:22:24 -0600

    Meowschwitz Switched Mixer Switched to Anim

--- Meowschwitz/Assets/Scripts/Traps/Mixer.cs
+++ Meowschwitz/Assets/Scripts/Traps/Mixer.cs
@@ -4,33 +4,17 @@ public class Mixer : MonoBehaviour
 {
 	
 	private ParticleSystem shiny;
-	private Transform bowl;
-	public float RotationAmount = 90f;
-	public bool go;
+	public Animator mixerAnim;
 
 	public void Start()
 	{
 		shiny = GetComponent<ParticleSystem>();
-		bowl = gameObject.transform.GetChild(0);
-		go = false;
+		mixerAnim = GetComponent<Animator>();
 	}
 	
 	public void Call()
 	{
 		shiny.Stop();
-		go = true;
-	}
-
-	private void Update()
-	{
-		if (go)
-		{
-			bowl.transform.Rotate (RotationAmount * Time.deltaTime * 2, 0, 0);
-		}
-
-		if (bowl.rotation.eulerAngles.x >= RotationAmount)
-		{
-			go = false;
-		}
+		mixerAnim.SetBool("Activate", true);
 	}
 }
