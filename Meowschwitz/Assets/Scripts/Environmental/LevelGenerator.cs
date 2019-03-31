0c6cb86c6aa942b5c055f48ff238aa80365af381
Unknown <kylehinckley@hotmail.com>
2019-03-31 10:26:06 -0600

    Meowschwitz Hunter Gen, Level Gen

---
+++ Meowschwitz/Assets/Scripts/Environmental/LevelGenerator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+	public GameObject[] LevelSlices;
+	public SOFloat NextX;
+	public float GenDistance;
+	public GameObject last, current, next;
+	public GameObject LevelGenPoint;
+
+	void Start ()
+	{
+		current = transform.GetChild(0).gameObject;
+		next = LevelSlices[Random.Range(0, LevelSlices.Length - 1)];
+		NextX.value = GenDistance;
+		LevelGenPoint.transform.position = new Vector3(GenDistance, 0, 0);
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			Debug.Log("Collided");
+			if (last != null)
+			{
+				Destroy(last);
+			}
+			
+			last = current;
+			current = next;
+			var InstanceSection = Instantiate(next, LevelGenPoint.transform);
+			InstanceSection.transform.parent = transform.parent;
+			Debug.Log("Instantiated");
+			
+			next = LevelSlices[Random.Range(0, LevelSlices.Length - 1)];
+			NextX.value += GenDistance;
+			LevelGenPoint.transform.position += new Vector3(NextX.Value, 0, 0);
+		}
+	}
+}
---
+++ Meowschwitz/Assets/Scripts/Traps/HunterGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using Ludiq;
+using UnityEngine;
+
+public class HunterGenerator : MonoBehaviour
+{
+
+	public GameObject Hunter;
+	public SOBool HunterPresent;
+	public float DelayTime;
+	private Transform startingPos;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player") && !HunterPresent.Value)
+		{
+			StartCoroutine(DelayedSpawn(DelayTime));
+		}
+	}
+	  
+	IEnumerator DelayedSpawn(float delayTime)
+	{
+		yield return new WaitForSeconds(delayTime);
+		Spawn();
+	}
+
+	private void Spawn()
+	{
+		var hunter = Instantiate(Hunter, transform);
+		hunter.transform.parent = transform.parent;
+		Destroy(gameObject.transform.GetChild(0).gameObject);
+	}
+}
