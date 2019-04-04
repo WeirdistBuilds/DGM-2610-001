64f30ae47d615075d4adf7d195db6e2aaaef649e
Unknown <kylehinckley@hotmail.com>
2019-04-04 18:36:48 +0800

    Meowschwitz Assets

--- Meowschwitz/Assets/Scripts/Environmental/LevelGenerator.cs
+++ Meowschwitz/Assets/Scripts/Environmental/LevelGenerator.cs
@@ -5,39 +5,34 @@ public class LevelGenerator : MonoBehaviour
 {
 
 	public GameObject[] LevelSlices;
-	public SOFloat NextX;
 	public float GenDistance;
 	public GameObject last, current, next;
-	public GameObject LevelGenPoint;
+	public GameObject StartingSection;
 
 	void Start ()
 	{
-		current = transform.GetChild(0).gameObject;
-		next = LevelSlices[Random.Range(0, LevelSlices.Length - 1)];
-		NextX.value = GenDistance;
-		LevelGenPoint.transform.position = new Vector3(GenDistance, 0, 0);
-		
+		current = StartingSection;
+		next = LevelSlices[Random.Range(0, LevelSlices.Length)];
+		StartingSection.SetActive(true);
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.CompareTag("Player"))
 		{
-			Debug.Log("Collided");
 			if (last != null)
 			{
-				Destroy(last);
+				last.SetActive(false);
 			}
 			
 			last = current;
 			current = next;
-			var InstanceSection = Instantiate(next, LevelGenPoint.transform);
+			transform.position += new Vector3(GenDistance, 0, 0);
+			var InstanceSection = Instantiate(next);
 			InstanceSection.transform.parent = transform.parent;
-			Debug.Log("Instantiated");
-			
-			next = LevelSlices[Random.Range(0, LevelSlices.Length - 1)];
-			NextX.value += GenDistance;
-			LevelGenPoint.transform.position += new Vector3(NextX.Value, 0, 0);
+			InstanceSection.transform.position = transform.position;
+			InstanceSection.SetActive(true);			
+			next = LevelSlices[Random.Range(0, LevelSlices.Length)];
 		}
 	}
 }
