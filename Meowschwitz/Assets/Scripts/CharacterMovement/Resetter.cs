1539223e2fb6a0719c929e6825dc62dfbf734293
Unknown <kylehinckley@hotmail.com>
2019-04-10 12:21:24 +0800

    Meowschwitz Area 6, 7, and 8

---
+++ Meowschwitz/Assets/Scripts/CharacterMovement/Resetter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Resetter : MonoBehaviour
+{
+
+	public SOBool ShouldJump, HunterPresent, IsDashing;
+	public SOFloat BaseSpeed;
+	public float SpeedReset;
+	
+	void Start ()
+	{
+		ShouldJump.Value = false;
+		HunterPresent.Value = false;
+		IsDashing.Value = false;
+		BaseSpeed.value = SpeedReset;
+	}
+}
